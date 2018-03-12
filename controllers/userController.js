27d93666e6d23c6879da91e614f66a9f064bfad1
crmgunter <crmgunter@gmail.com>
2018-03-12 17:27:23 -0400

    full crud on user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,63 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
-router.get('/', (req, res) => {
-    User.find().then((users) => {
-        res.json(users)
+// INDEX ROUTE
+router.get("/", (req, res) => {
+  User.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+// CREATE ROUTE
+router.post("/", (req, res) => {
+  const newUser = new User({
+    username: req.body.username
+  });
+  newUser.save().then(savedUser => {
+    res.redirect(`/api/users`);
+  });
+});
+
+//SHOW ROUTE
+router.get("/:id", (req, res) => {
+    const userId = req.params.id;
+    const user = User.findById(userId).then((user) => {
+      res.json(user);
+    }).catch(err => {
+    console.log(err);
+    res.json(err);
+  });
+})
+
+//UPDATE ROUTE
+router.patch('/:id', (req, res) => {
+    const userId = req.params.id
+    const updatedUser = req.body
+    const savedUser = User.findByIdAndUpdate(userId, updatedUser)
+    .then((updatedUser) => {
+        res.json(savedUser)
+        res.redirect('/api/users')
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+//DELETE ROUTE
+router.delete('/:id', (req, res) => {
+    const userId = req.params.id
+    User.findByIdAndRemove(userId)
+    .then(() => {
+        res.redirect('/api/users')
     }).catch((err) => {
         console.log(err)
     })
 })
 
-router.post('/', (req, res) => {
-    const newUser = new User({
-        username: req.body.username
-    })
-    newUser.save().then((savedUser) => {
-        res.redirect(`/api/user`)
-    })
-})
 
-module.exports = router
+module.exports = router;
