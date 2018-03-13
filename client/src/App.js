90c1a11025b231ef34ba7bb3e984a1835dd45b35
crmgunter <crmgunter@gmail.com>
2018-03-12 22:10:13 -0400

    navbar up and running

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Users from './components/Users'
 import SingleUser from './components/SingleUser'
-
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <Router>
       <div>
+        <NavBar/>
         <Switch>
           <Route exact path='/users' component={Users}/>
           <Route exact path='/users/:userId' component={SingleUser}/>
--- client/src/components/NewUserForm.js
+++ client/src/components/NewUserForm.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from 'axios'
 
 class NewUserForm extends Component {
-    render() {
-        return (
-            <div>
-                <h1>hey</h1>
-            </div>
-        );
-    }
+  state = {
+    username: ''
+  };
+
+  handleChange = (event) => {
+      const username = event.target.username
+      const newState = {...this.state}
+      newState[username] = event.target.value
+      console.log(newState)
+      this.setState(newState)
+      console.log(newState[username])
+  }
+
+  handleSubmit = async (event) => {
+      event.preventDefault()
+      const payload = {
+          username: this.state.username
+      }
+      await axios.post('/api/users', payload)
+      await this.props.getAllUsers()
+      console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hey</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="username">Username: </label>
+            <input onChange={this.handleChange} type="text" name="username" value={this.state.username} />
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewUserForm;
