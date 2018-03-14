9129c0e67bad965831e4a7f47d1698fe1fd3602c
crmgunter <crmgunter@gmail.com>
2018-03-14 16:46:39 -0400

    setting up card page

--- client/src/components/SingleCard.js
+++ client/src/components/SingleCard.js
@@ -4,26 +4,37 @@ import axios from 'axios'
 class SingleCard extends Component {
     state = {
         user: {},
-        deck: {
-            cards:[]
-        }
+        deck: {},
+        card: {}
       };
     
       async componentWillMount() {
-        const userId = this.props.match.params.userId;
+        const userId = this.props.match.params.userdId;
+        console.log(userId)
         const deckId = this.props.match.params.deckId;
         const cardId = this.props.match.params.cardId;
+        console.log(cardId)
         const res = await axios.get(`/api/users/${userId}/decks/${deckId}`);
         const userRes = await axios.get(`/api/users/${userId}`);
+        const cardRes = await axios.get(`/api/users/${userId}/decks/${deckId}/cards/${cardId}`)
+        console.log(cardRes)
         const user = userRes.data;
         const deck = res.data;
-        this.setState({ user, deck });
+        const card = cardRes.data;
+        this.setState({ user, deck, card });
       }
 
     render() {
         return (
             <div>
                 <h1>hey from single Card!</h1>
+                <div>{this.state.card.name}</div>
+                <div><img src={this.state.card.img} alt="card"/></div>
+                <div>{this.state.card.color}</div>
+                <div>{this.state.card.cmc}</div>
+                <div>{this.state.card.rarity}</div>
+                <div>{this.state.card.set}</div>
+                <div>{this.state.card.artist}</div>
             </div>
         );
     }
