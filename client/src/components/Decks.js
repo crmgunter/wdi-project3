3aa8aa9678046c2d596e0d0c6cabd3c19044a7fd
crmgunter <crmgunter@gmail.com>
2018-03-13 22:18:27 -0400

    working on add new deck function on front end. not working currently. basing it on previous new user form but may be barking up the wrong tree. calling it quits for the night

--- client/src/components/Decks.js
+++ client/src/components/Decks.js
@@ -1,38 +1,47 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
+import NewDeckForm from './NewDeckForm'
 
 class Decks extends Component {
-    state = {
-        user: {
-            decks: []
-        }
-    }
+  state = {
+    user: {
+      decks: []
+    },
+    newDeck: false
+  };
 
-    async componentWillMount() {
-        const userId = this.props.match.params.userId;
-        const deckId = this.props.match.params.deckId
-        const res = await axios.get(`/api/users/${userId}`);
-        const user = res.data;
-        this.setState({ user });
-        console.log(this.state.user.decks);
-      }
+  async componentWillMount() {
+    const userId = this.props.match.params.userId;
+    const res = await axios.get(`/api/users/${userId}`);
+    const user = res.data;
+    this.setState({ user });
+    console.log(this.state.user.decks);
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Hey from decks!</h1>
-                {this.state.user.username}'s decks
-                {this.state.user.decks.map(deck => (
-                    <div key={deck._id}>
-                    <h3>{deck.name}</h3>
-                    <h5>{deck.description}</h5>
-                    <h5>{deck.archetype}</h5>
-                    <h5>{deck.format}</h5>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+  toggleNewDeckForm = () => {
+      this.setState({ newDeck: !this.state.newDeck})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hey from decks!</h1>
+        <div>
+          <button onClick={this.toggleNewDeckForm}>Add New Deck</button>
+          {this.state.newDeck ? <NewDeckForm/> : null}
+        </div>
+        {this.state.user.username}'s decks
+        {this.state.user.decks.map(deck => (
+          <div key={deck._id}>
+            <h3>{deck.name}</h3>
+            <h5>{deck.description}</h5>
+            <h5>{deck.archetype}</h5>
+            <h5>{deck.format}</h5>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Decks;
--- client/src/components/NewDeckForm.js
+++ client/src/components/NewDeckForm.js
@@ -1,13 +1,90 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class NewDeckForm extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hey!</h1>
-            </div>
-        );
+  state = {
+    user: {
+      name: "",
+      description: "",
+      archetype: "",
+      format: ""
     }
+  };
+
+  componentWillMount() {
+    this.getAllDecks();
+  }
+  getAllDecks = async () => {
+    const res = await axios.get("/api/users/:userId/decks");
+    this.setState({ user: res.data });
+  };
+
+  handleChange = event => {
+    const name = event.target.name;
+    const newState = { ...this.state };
+    newState[name] = event.target.value;
+    this.setState(newState);
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const payload = {
+      name: this.state.name,
+      description: this.state.description,
+      archetype: this.state.archetype,
+      format: this.state.format
+    };
+    console.log(this.state);
+    await axios.post("/api/users/:userId/decks", payload);
+    await this.getAllDecks()
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hey!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="name">name: </label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              value={this.state.name}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description: </label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="description"
+              value={this.state.description}
+            />
+          </div>
+          <div>
+            <label htmlFor="archetype">archetype: </label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="archetype"
+              value={this.state.archetype}
+            />
+          </div>
+          <div>
+            <label htmlFor="format">format: </label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              name="format"
+              value={this.state.format}
+            />
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewDeckForm;
